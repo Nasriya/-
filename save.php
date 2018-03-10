02d13025338a4ad9539812d62193de338a3ab996
Nasriya <nasriya.pop@gmail.com>
2018-03-10 18:26:01 +0700

    ก

--- save.php
+++ save.php
@@ -1,36 +1,19 @@
 <?
 session_start();
-if (isset($_POST['Edit'])) {
-	$Username = $_POST['Username'];
-	$Firstname = $_POST['Firstname'];
-	$Lastname = $_POST['Lastname'];
-	$Telephone = $_POST['Telephone'];
-	$Address = $_POST['Address'];
-	$Password = ($_POST['Password']);
-
-	mysqli_query("UPDATE user SET Firstname='$Firstname', Address='$Address' WHERE Member_ID='{$_SESSION['UserID']}'");
-	$_SESSION['message'] = "Address updated!";
-	header('location: index.php');
-}
 
 
-
-
-
-
-}
 
 				//query
 
-                $query="UPDATE user SET Firstname='{$Firstname}', Lastname='{$Lastname}, Username='{$Username}', Address='{$Address}' WHERE Member_ID='{$_SESSION['UserID']}' ";
-                 $result = mysqli_query($con,$query);
+                $sql="UPDATE user SET
+								Firstname='".$_POST["Firstname"]."',
+								Lastname='".$_POST["Lastname"]."',
+								Username='".$_POST["Username"]."',
+								Address='".$_POST["Address"]."',
+								WHERE Member_ID='{$_SESSION['UserID']}' ";
+                 $query = mysqli_query($con,$sql);
+								 if($query) {
 
-                  if(mysqli_num_rows($result)==1){
+echo "Record update successfully";
 
-                      $row = mysqli_fetch_array($result);
-
-                      $_SESSION["UserID"] = $row["Member_ID"];
-                      $_SESSION["User"] = $row["Firstname"]." ".$row["Lastname"];
-                      $_SESSION["Userlevel"] = $row["Userlevel"];
-										}
-									}
+}
--- show.php
+++ show.php
@@ -1,17 +1,64 @@
-<html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+	<meta charset="utf-8">
+	<title>ข้อมูลผู้ใช้</title>
+	<meta name="description" content="Free Responsive Html5 Css3 Templates | html5xcss3.com">
+	<meta name="author" content="www.html5xcss3.com">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="css/zerogrid.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/lightbox.css">
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/menu.css">
+<link rel="stylesheet" href="css/form.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<body>
+<body class="home-page">
+	<div class="wrap-body">
+		<div class="header">
+			<div id='cssmenu' >
+				<ul>
+				   <li><img src="images/f.png" width="150" height="150"></li>
+					 <li><a href='employee.php'><span>จัดการคิว</span></a></li>
+				     <li><a href='check.php'><span>ตรวจสอบ</span></a></li>
+
+
+ 					<button class="w3-button w3-round-xlarge w3-white w3-display-topright" style="width:150px"><a href='logout.php'>ออกจากระบบ</a></button>
+				</ul>
+			</div>
+		</div>
+
+
 <?php
-include('connection.php');
-$id = $_GET['Member_ID'];
-$sql = "SELECT * FROM user WHERE Member_ID='$id' ";
-$result = mysqli_query($con,$sql);
-$data = mysqli_fetch_array($result);
+//1. เชื่อมต่อ database:
+include('connection.php');  //ไฟล์เชื่อมต่อกับ database ที่เราได้สร้างไว้ก่อนหน้าน้ี
+
+//2. query ข้อมูลจากตาราง tb_member:
+$query = "SELECT * FROM user ORDER BY Member_ID asc" or die("Error:" . mysqli_error());
+//3.เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result .
+$result = mysqli_query($con, $query);
+//4 . แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล:
+echo "<table border='1' align='center' width='1000'>";
+//หัวข้อตาราง
+echo "<tr align='center' bgcolor='#ffffff'><td>รหัส</td><td>Uername</td><td>ชื่อ</td><td>นามสกุล</td><td>Telephone</td><td>Email</td><td>แก้ไข</td><td>ลบ</td></tr>";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" .$row["Member_ID"] .  "</td> ";
+  echo "<td>" .$row["Username"] .  "</td> ";
+  echo "<td>" .$row["Firstname"] .  "</td> ";
+  echo "<td>" .$row["Lastname"] .  "</td> ";
+  echo "<td>" .$row["Telephone"] .  "</td> ";
+  echo "<td>" .$row["Email"] .  "</td> ";
+  //แก้ไขข้อมูล
+  echo "<td><a href='UserUpdateForm.php?ID=$row[0]'>edit</a></td> ";
+
+  //ลบข้อมูล
+  echo "<td><a href='UserDelete.php?ID=$row[0]' onclick=\"return confirm('Do you want to delete this record? !!!')\">del</a></td> ";
+  echo "</tr>";
+}
+echo "</table>";
+//5. close connection
+mysqli_close($con);
 ?>
-<a href="detail.php?id=<?=$data['Member_ID'];?>">ดูข้อมูลส่วนตัว</a>
-</body>
-</html>
