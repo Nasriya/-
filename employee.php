4f89cdc38b7fd327cd374dd273dbc128eacca6ca
Nasriya <nasriya.pop@gmail.com>
2018-03-13 19:33:20 +0700

    d

--- employee.php
+++ employee.php
@@ -73,8 +73,7 @@ if (!$_SESSION["UserID"]){  //check session
 			//4 . แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล:
 			echo "<table align='center' width='900' >";
 			//หัวข้อตาราง
-			echo " <tr border='1' align='center'>
-
+			echo " <tr align='center'>
 			<td bgcolor='#61b4cf'><br>Member_ID</br></td>
 			<td bgcolor='#b8b4b0'><br>เบอร์โทรศัพท์</br></td>
 			<td bgcolor='#61b4cf'><br>ชื่อสมาชิก</br></td>
@@ -84,9 +83,6 @@ if (!$_SESSION["UserID"]){  //check session
 			<td bgcolor='#b8b4b0'><br>สถานะ</br></td>
 			<td bgcolor='#61b4cf'><br></br></td>
 			<td bgcolor='#61b4cf'><br>ลบ</br></td>
-
-
-
 			<td></br></br></td>
 			<td><br></br></td></tr>";
 
@@ -95,8 +91,8 @@ if (!$_SESSION["UserID"]){  //check session
 			  echo "<td align='center'  bgcolor='#def1f9'>" .$row["Member_ID"] .  "</td> ";
 				echo "<td align='center'  bgcolor='#e1dedc'>" .$row["Telephone"] .  "</td> ";
 			  echo "<td align='center'  bgcolor='#def1f9'>" .$row["Username"] .  "</td> ";
-			  echo "<td align='center'  bgcolor='#e1dedc'><a href='#.php?ID=$row[0]'>" .$row["fileupload"] .  "</td> ";
-			  echo "<td align='center'  bgcolor='#def1f9'><a href='#.php?ID=$row[0]'>รายละเอียด" .  "</td> ";
+			  echo "<td align='center'  bgcolor='#e1dedc'><a href='file.php?ID=$row[0]'>" .$row["fileupload"] .  "</td> ";
+			  echo "<td align='center'  bgcolor='#def1f9'><a href='note.php?ID=$row[0]'>รายละเอียด" .  "</td> ";
 				echo "<td align='center'  bgcolor='#def1f9'>" ."</td> ";//ลบ
 				echo "<td align='center' bgcolor='#e1dedc' <td><select name='P_Status'>
 				<option selected>--สถานะ--</option>
@@ -104,18 +100,16 @@ if (!$_SESSION["UserID"]){  //check session
 				<option>กำลังดำเนินงาน</option>
 			 <option>รอการชำระ</option>
 			 <option>ชำระเเล้ว</option>
-			 </select><td>";//สถานะ
+			 </select>&nbsp;&nbsp;&nbsp;&nbsp;<button class='w3-button w3-small w3-green' style='width:55px'>Save</button><td>";//สถานะ
 
 			//ลบข้อมูล
-			  echo "<td align='center' bgcolor='#def1f9'><a href='UserDelete.php?ID=$row[0]' onclick=\"return confirm('Do you want to delete this record? !!!')\">ลบคำสั่งซื้อ</a></td> ";
+			  echo "<td align='center' bgcolor='#def1f9'><a href='UserDelete.php?ID=$row[0]'><button class='w3-button w3-small w3-red' style='width:60px'>Delete</button></a></td> ";
 			  echo "</tr>";
 			}
 			echo "</table>";
 			//5. close connection
 			mysqli_close($con);
 }
-
-
-
-
 			?>
+			<br></br>
+			<center><button class="w3-button w3-round-xlarge w3-red" style="width:200px">ล้างข้อมูลทั้งหมด</button></center>
---
+++ note.php
@@ -0,0 +1,119 @@
+<?php session_start();?>
+<?php
+
+include('connection.php');
+
+if (!$_SESSION["UserID"]){  //check session
+
+	  Header("Location: form_login.php"); //ไม่พบผู้ใช้กระโดดกลับไปหน้า login form
+
+}else{
+	//$_SESSION['frmAction'] = md5('itoffside.com' . rand(1, 9999));
+?>
+
+<?php
+
+
+$meSQL = "SELECT * FROM uploadfile WHERE Member_ID='{$_SESSION['UserID']}' ";
+$meQuery = mysqli_query($con,$meSQL);
+if ($meQuery == TRUE) {
+$meResult = mysqli_fetch_assoc($meQuery);
+} else {
+echo 'error';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>สรุปผลการสั่งซื้อ</title>
+	<meta name="description" content="Free Responsive Html5 Css3 Templates | html5xcss3.com">
+	<meta name="author" content="www.html5xcss3.com">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="css/zerogrid.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/lightbox.css">
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/menu.css">
+<link rel="stylesheet" href="css/form.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="js/jquery1111.min.js" type="text/javascript"></script>
+	<script src="js/script.js"></script>
+
+</head>
+
+<body class="home-page">
+	<div class="wrap-body">
+		<div class="header">
+			<div id='cssmenu' >
+				<ul>
+				   <li><img src="images/f.png" width="150" height="150"></li>
+				     <li><a href='index.php'><span>หน้าแรก</span></a></li>
+						<li><a href='service.php'><span>บริการของเรา</span></a></li>
+				   <li><a href='contact.php'><span>ติดต่อเรา</span></a></li>
+					 <li><a href='upload.php'><span>อัพโหลดไฟล์</span></a></li>
+
+ 					<button class="w3-button w3-round-xlarge w3-white w3-display-topright" style="width:150px"><a href='logout.php'>ออกจากระบบ</a></button>
+				</ul>
+			</div>
+		</div>
+
+<center>
+
+		<div class="container" >
+
+<br></br>
+				<div class="w3-round-small w3-border w3-sand" style="width:40%" >
+          <table class="w3-table w3-bordered">
+            <center>
+
+              <b>สรุปการสั่งซื้อ</b>
+<br></br>
+            <tr>
+              <td>ไฟล์งาน :</td>
+              <td><a href="#" ><?php echo $meResult['fileupload']; ?></td>
+            </tr>
+            <tr>
+              <td>ประเภทการสั่งพิมพ์ :</td>
+              <td><?php echo $meResult['ProductType']; ?></td>
+            </tr>
+            <tr>
+              <td>ขนาดกระดาษ :</td>
+              <td><?php echo $meResult['ProductDetail']; ?></td>
+            </tr>
+            <tr>
+              <td>จำนวน :</td>
+              <td><?php echo $meResult['Quanitity']; ?></td>
+            </tr>
+            <tr>
+              <td>วันที่นัดรับ :</td>
+              <td><?php echo $meResult['DateReceip']; ?></td>
+
+            </tr>
+            <tr>
+              <td>เวลาที่นัดรับ :</td>
+              <td><?php echo $meResult['TimeReceip']; ?></td>
+            </tr>
+            <tr>
+              <td>ราคารวม :</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>เเต้มสะสม :</td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+
+<br></br>
+
+    <a href="upload.php" class="w3-btn  w3-round-xxlarge w3-red" style="width:200px">close</a>
+
+
+</div>
+</div>
+</body>
+</html>
+<?php }?>
